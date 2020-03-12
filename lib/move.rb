4629f5a18f39f46161cec1c0065e4161ed1fd4c8
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-03-12 19:56:30 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -10,6 +10,6 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(input_to_index)
-  display_board(input_to_index)
+def move(array, index, value)
+  board[input_to_index] = value
 end
