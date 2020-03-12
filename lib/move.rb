2c601350a5fb5ad18b28f68d2a1f8abe8111f686
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-03-12 19:40:53 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,6 +6,6 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(int)
-  converted_input = int - 1
+def input_to_index(user_input)
+  converted_input = user_input - 1
 end
