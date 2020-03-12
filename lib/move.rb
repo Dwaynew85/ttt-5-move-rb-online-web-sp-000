4878ec6302c52d81e158579e4d179e046226dc73
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-03-12 20:08:54 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,11 +6,10 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def update_array_at_with(array, index, value)
-  array[index] = value
-end
-
-update_array_at_with(board, 0, "X")
 def input_to_index(user_input)
   user_input.to_i - 1
+end
+
+def move(board, index, player = "X")
+  board[index] = player
 end
